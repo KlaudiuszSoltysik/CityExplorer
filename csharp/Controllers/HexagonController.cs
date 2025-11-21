f695e04fded924092994b701e91deb24fdc384cb
KlaudiuszSoltysik <klaudiusz.s1405@gmail.com>
2025-11-21 17:35:30 +0100

    ok

---
+++ csharp/Controllers/HexagonController.cs
@@ -0,0 +1,31 @@
+﻿using csharp.Dtos;
+using csharp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace csharp.Controllers;
+
+[Route("hexagon")]
+[ApiController]
+public class HexagonController(PostgresContext postgresContext) : ControllerBase
+{
+    [HttpGet("get-hexagons-from-city")]
+    public async Task<IActionResult> GetHexagonsFromCity([FromQuery] string mode, [FromQuery] string city)
+    {
+        if (mode != "tourist" && mode != "local")
+        {
+            return BadRequest("Invalid mode specified. Must be 'tourist' or 'local'.");
+        }
+
+        var hexagonsData = await postgresContext.Hexagons
+            .Where(h => h.City == city)
+            .Select(h => new GetHexagonsFromCityDto
+            {
+                Id = h.Id,
+                Weight = mode == "tourist" ? h.TouristWeight : h.LocalWeight
+            })
+            .ToListAsync();
+
+        return Ok(hexagonsData);
+    }
+}
---
+++ csharp/Dtos/HexagonDtos.cs
@@ -0,0 +1,7 @@
+﻿namespace csharp.Dtos;
+
+public class GetHexagonsFromCityDto
+{
+    public string Id { get; set; }
+    public double Weight { get; set; }
+}
