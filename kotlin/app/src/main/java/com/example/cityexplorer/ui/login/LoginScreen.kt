3c01fb18835fc421fc7eb8d90453174a6919ec81
KlaudiuszSoltysik <klaudiusz.s1405@gmail.com>
2025-11-28 23:34:47 +0100

    feat: enhance LoginScreen with Google login button and icon

--- kotlin/app/src/main/java/com/example/cityexplorer/ui/login/LoginScreen.kt
+++ kotlin/app/src/main/java/com/example/cityexplorer/ui/login/LoginScreen.kt
@@ -3,9 +3,11 @@ package com.example.cityexplorer.ui.login
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -16,8 +18,17 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.cityexplorer.ui.theme.CustomBlack
 import com.example.cityexplorer.ui.theme.CustomError
 import com.example.cityexplorer.ui.theme.Primary
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.cityexplorer.R
+import com.example.cityexplorer.ui.theme.CustomWhite
 
 @Composable
 fun LoginScreen(
@@ -36,12 +47,34 @@ fun LoginScreen(
             is MainUiState.Loading -> CircularProgressIndicator()
 
             is MainUiState.Waiting -> {
-                Button(onClick = {
-                    // Tutaj wkrótce wstawimy prawdziwy kod Google!
-                    val fakeToken = "ey...twoj_token_jwt..."
-                    viewModel.onGoogleLoginSuccess(fakeToken, onNavigateToCitySelectorScreen)
-                }) {
-                    Text("Login with Google")
+                Button(
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = CustomWhite,
+                    ),
+                    shape = RoundedCornerShape(8.dp),
+                    contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+                    onClick = {
+                        // Tutaj wkrótce wstawimy prawdziwy kod Google!
+                        val fakeToken = "ey...twoj_token_jwt..."
+                        viewModel.onGoogleLoginSuccess(fakeToken, onNavigateToCitySelectorScreen)
+                    }
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_google_logo),
+                            contentDescription = "Google Logo",
+                            tint = Color.Unspecified,
+                            modifier = Modifier.size(32.dp)
+                        )
+                        Spacer(modifier = Modifier.width(12.dp))
+                        Text(
+                            text = "Login with Google",
+                            color = CustomBlack,
+                            fontSize = 20.sp,
+                        )
+                    }
                 }
             }
 
@@ -55,7 +88,6 @@ fun LoginScreen(
                     Button (
                         colors = ButtonDefaults.buttonColors(
                             containerColor = Primary,
-//                            contentColor = CustomWhite
                         ),
                         shape = RoundedCornerShape(8.dp),
                         contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
